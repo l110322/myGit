ff2db1465f06d636f7acd418bf2cb54ac47de6e1
l110322 <1103226752@qq.com>
2016-01-09 23:41:40 +0800

    install_package.sh
    ee

---
+++ install_package.sh
@@ -0,0 +1,26 @@
+apt-get install dos2unix
+apt-get install mcl
+
+apt-get install python-scipy
+apt-get install python-rpy2
+apt-get install python-matplotlib
+
+
+apt-get install python-pip
+apt-get install python-dev
+pip install numpy
+pip install pandas
+pip install matplotlib
+
+# wget http://www.netlib.org/lapack/lapack-3.6.0.tgz
+# tar zxvf lapack-3.6.0.tgz 
+# cd lapack-3.6.0/
+# cp make.inc.example make.inc
+# vi Makefile
+# apt-get install gfortran
+# make
+# cp lib*.a /usr/lib
+
+#apt-get install bedtools
+#bedtools closetBed
+#apt-get install lib32stdc++6
